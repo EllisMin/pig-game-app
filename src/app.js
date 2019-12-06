a61ebd1e5911c05bd6b30c1cdcc3b871c273a6e5
Ellis Min <dbmin@hotmail.com>
2019-12-05 23:47:31 -0600

    added animation on dice 1

--- src/app.js
+++ src/app.js
@@ -1,51 +1,36 @@
 var scores = [0, 0];
 var curTotalScore = 0;
 var activePlayer = 0;
-
-/* Change current dice # */
-// textContent() gets the content of all elements, including <script> and <style> elements
-// document.querySelector("#cur-" + activePlayer).textContent = dice;
-
-// In contrast, innerText() only shows “human-readable” elements
-// document.querySelector("#cur-" + activePlayer).innerHTML =
-//   "<em>" + dice + "</em>";
-
-// Also possible to access  like this
-// var x = document.querySelector("#score-0").innerHTML;
-// console.log(x);
-
 // Changing CSS property
 document.querySelector(".dice-img").style.display = "none";
 
-// Event listener for roll btn
-// btn is a callback fcn; fun passed as an argument
-// document.querySelector(".btn-roll").addEventListener("click", btn);
-// function btn() {}
-
-// Anonymous function; function that's used only here; has no usability
 document.querySelector(".btn-roll").addEventListener("click", function() {
+  var diceDOM = document.querySelector(".dice-img");
+  // Remove animation
+  diceDOM.style.animation = "none";
+
   // 1. get rand# btwn 1 - 6
   var dice = Math.floor(Math.random() * 6) + 1;
 
   // 2. display dice with rand generated#
-  var diceDOM = document.querySelector(".dice-img");
   diceDOM.style.display = "block";
   diceDOM.src = "./img/" + dice + ".png";
 
   // 3. update curTotalScore if rolled# wasn't 1
   if (dice === 1) {
+    // Fade in effect
+    diceDOM.style.animation = "fadeIn 1.5s forwards ease-in";
+
     switchPlayer();
   } else {
     curTotalScore += dice;
-
     // Selects by id. Faster; exclude '#'
     document.getElementById("cur-" + activePlayer).innerHTML = curTotalScore;
   }
 });
 
+// Hold Btn
 document.querySelector(".btn-hold").addEventListener("click", function() {
-//   console.log(scores); ///
-
   // Update score
   scores[activePlayer] += curTotalScore;
   document.getElementById("score-" + activePlayer).innerHTML =
@@ -59,5 +44,12 @@ function switchPlayer() {
   curTotalScore = 0;
   document.getElementById("cur-" + activePlayer).innerHTML = 0;
 
+  // Update active class & update user
+  document
+    .querySelector(".player-" + activePlayer + "-panel")
+    .classList.remove("active");
   activePlayer = activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  document
+    .querySelector(".player-" + activePlayer + "-panel")
+    .classList.add("active");
 }

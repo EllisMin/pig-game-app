788be7b07a812eef6bd871cec5299eb4f82fff72
Ellis Min <dbmin@hotmail.com>
2019-12-06 00:33:54 -0600

    finished new game function

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
-var scores = [0, 0];
-var curTotalScore = 0;
-var activePlayer = 0;
-// Changing CSS property
-document.querySelector(".dice-img").style.display = "none";
+var scores, curTotalScore, activePlayer, isPlaying;
+
+var winningScore = 15; // inclusive
+init();
 
 document.querySelector(".btn-roll").addEventListener("click", function() {
   var diceDOM = document.querySelector(".dice-img");
@@ -13,6 +12,7 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
   var dice = Math.floor(Math.random() * 6) + 1;
 
   // 2. display dice with rand generated#
+
   diceDOM.style.display = "block";
   diceDOM.src = "./img/" + dice + ".png";
 
@@ -24,7 +24,6 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
     switchPlayer();
   } else {
     curTotalScore += dice;
-    // Selects by id. Faster; exclude '#'
     document.getElementById("cur-" + activePlayer).innerHTML = curTotalScore;
   }
 });
@@ -35,8 +34,20 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
   scores[activePlayer] += curTotalScore;
   document.getElementById("score-" + activePlayer).innerHTML =
     scores[activePlayer];
+  // Winning score achieved
+  if (scores[activePlayer] >= winningScore) {
+    var nameDOM = document.getElementById("name-" + activePlayer);
+    nameDOM.innerHTML = "Winner!";
+    nameDOM.classList.add("winner-text");
 
-  switchPlayer();
+    // isPlaying = false;
+    // Hide UIs
+    document.querySelector(".btn-roll").style.display = "none";
+    document.querySelector(".btn-hold").style.display = "none";
+    document.querySelector(".dice-img").style.display = "none";
+  } else {
+    switchPlayer();
+  }
 });
 
 function switchPlayer() {
@@ -44,12 +55,38 @@ function switchPlayer() {
   curTotalScore = 0;
   document.getElementById("cur-" + activePlayer).innerHTML = 0;
 
-  // Update active class & update user
-  document
-    .querySelector(".player-" + activePlayer + "-panel")
-    .classList.remove("active");
+  // Update user
   activePlayer = activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-  document
-    .querySelector(".player-" + activePlayer + "-panel")
-    .classList.add("active");
+
+  // Update active class, alternative to remove/add class
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+}
+
+// New Game
+document.querySelector(".btn-new").addEventListener("click", init);
+
+function init() {
+  // Reset Score
+  scores = [0, 0];
+  curTotalScore = 0;
+  if (activePlayer !== undefined) {
+    var nameDOM = document.getElementById("name-" + activePlayer);
+    nameDOM.innerHTML = "player " + (activePlayer + 1);
+    nameDOM.classList.remove("winner-text");
+  }
+  activePlayer = 0;
+  // State variable
+  // isPlaying = true;
+
+  // handle UI
+  document.querySelector(".btn-roll").style.display = "inline";
+  document.querySelector(".btn-hold").style.display = "inline";
+  document.querySelector(".dice-img").style.display = "none";
+  document.querySelector(".player-0-panel").classList.add("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("cur-0").textContent = "0";
+  document.getElementById("cur-1").textContent = "0";
 }
